7ccc87dd184a34d23f0986a806cfd599930b9f2c
digimokan <digimokan@users.noreply.github.com>
2019-01-11 23:17:17 -0600

    Add Square class, a Sq containing a SquareVal

---
+++ src/Board/Square.cpp
@@ -0,0 +1,40 @@
+/*******************************************************************************
+module:   Square
+author:   digimokan
+date:     11 JAN 2019
+purpose:  a board square, which can contain SquareVal (1-8, or empty)
+*******************************************************************************/
+
+/*******************************************************************************
+* USER INCLUDES
+*******************************************************************************/
+
+#include "Square.hpp"
+#include "SquareVal.hpp"
+
+/*******************************************************************************
+* CONSTRUCTORS
+*******************************************************************************/
+
+Square::Square ()
+  : val{SquareVal::EMPTY} { }
+
+Square::Square (SquareVal val)
+  : val{val} { }
+
+/*******************************************************************************
+* SPECIALIZED METHODS
+*******************************************************************************/
+
+SquareVal Square::get_val () const {
+  return this->val;
+}
+
+void Square::set_val (SquareVal in_val) {
+  this->val = in_val;
+}
+
+bool Square::is_empty () const {
+  return (this->val == SquareVal::EMPTY);
+}
+
---
+++ src/Board/Square.hpp
@@ -0,0 +1,55 @@
+/*******************************************************************************
+module:   Square
+author:   digimokan
+date:     11 JAN 2019
+purpose:  a board square, which can contain SquareVal (1-8, or empty)
+*******************************************************************************/
+
+#ifndef SQUARE_HPP
+#define SQUARE_HPP 1
+
+/*******************************************************************************
+* FORWARD DECLARES
+*******************************************************************************/
+
+enum class SquareVal;
+
+/*******************************************************************************
+* INTERFACE
+*******************************************************************************/
+
+class Square {
+
+public:
+
+  // constructors
+  Square ();
+  explicit Square (SquareVal);
+
+  // destructor
+  ~Square () = default;
+
+  // operators
+  Square (const Square& in) = default;
+  Square& operator= (const Square& rh) = default;
+  Square (Square&& in) = default;
+  Square& operator= (Square&& rh) = default;
+
+  // specialized methods
+  SquareVal get_val () const;
+  void set_val (SquareVal);
+  bool is_empty () const;
+
+private:
+
+  // fields
+  SquareVal val;
+
+};
+
+/*******************************************************************************
+* END
+*******************************************************************************/
+
+#endif // SQUARE_HPP
+
