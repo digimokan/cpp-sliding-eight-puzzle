9eccff13399c57d43cde00b384a3fd010702ed89
digimokan <digimokan@users.noreply.github.com>
2019-01-11 23:14:21 -0600

    Add SquareVal enum class to represent 1-8/empty

---
+++ src/Board/SquareVal.hpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+module:   SquareVal
+author:   digimokan
+date:     11 JAN 2019
+purpose:  value of a board square (1-8, or empty)
+*******************************************************************************/
+
+enum class SquareVal {
+  EMPTY,
+  ONE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT
+};
+
