95bfabe2739ff5caf8b693b8ebde9e04ce981ee0
digimokan <digimokan@users.noreply.github.com>
2019-01-13 20:23:21 -0600

    Add MoveDir enum class (up, down, left, right)

---
+++ src/Board/MoveDir.hpp
@@ -0,0 +1,14 @@
+/*******************************************************************************
+module:   MoveDir
+author:   digimokan
+date:     13 JAN 2019
+purpose:  a possible direction to move the empty (0) square of a board
+*******************************************************************************/
+
+enum class MoveDir {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT
+};
+
