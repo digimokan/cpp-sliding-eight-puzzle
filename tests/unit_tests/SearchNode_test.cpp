a9cc289ec0f256910c17769574c41ff68b185d29
digimokan <digimokan@users.noreply.github.com>
2019-01-15 23:47:37 -0600

    Add SearchNode unit test: deep, multibranch tree

--- tests/unit_tests/SearchNode_test.cpp
+++ tests/unit_tests/SearchNode_test.cpp
@@ -90,3 +90,106 @@ TEST_CASE("create tree with three nodes: root/child/grandchild") {
 
 }
 
+TEST_CASE("create multi-branch, multi-level tree") {
+/*******************************************************************************
+        A
+      / |
+     B  C
+       / \
+      D  E
+      |
+      F
+*******************************************************************************/
+
+  std::shared_ptr<SearchNode> A{ std::make_shared<SearchNode>(Board{"012345678"}) };
+  std::shared_ptr<SearchNode> B{ std::make_shared<SearchNode>(Move{Board{"102345678"}, MoveDir::RIGHT, 1}, A) };
+  std::shared_ptr<SearchNode> C{ std::make_shared<SearchNode>(Move{Board{"312045678"}, MoveDir::DOWN, 3}, A) };
+  std::shared_ptr<SearchNode> D{ std::make_shared<SearchNode>(Move{Board{"312405678"}, MoveDir::RIGHT, 4}, C) };
+  std::shared_ptr<SearchNode> E{ std::make_shared<SearchNode>(Move{Board{"312645078"}, MoveDir::DOWN, 6}, C) };
+  std::shared_ptr<SearchNode> F{ std::make_shared<SearchNode>(Move{Board{"302415678"}, MoveDir::UP, 1}, D) };
+  A->add_child(B);
+  A->add_child(C);
+  C->add_child(D);
+  C->add_child(E);
+  D->add_child(F);
+
+  SUBCASE("A-node fields valid") {
+    CHECK_UNARY_FALSE(A->get_parent());
+    CHECK_EQ(A->get_depth(), 0);
+    CHECK_EQ(A->get_height(), 3);
+    CHECK_UNARY(A->get_board() == Board{"012345678"});
+    CHECK_UNARY_FALSE(A->get_move_dir());
+    CHECK_EQ(A->get_move_cost(), 0);
+    CHECK_EQ(A->get_path_cost(), 0);
+    CHECK_UNARY(A->is_root());
+    CHECK_UNARY(A->has_children());
+    CHECK_UNARY_FALSE(A->is_leaf());
+  }
+
+  SUBCASE("B-node fields valid") {
+    CHECK_EQ(B->get_parent(), A);
+    CHECK_EQ(B->get_depth(), 1);
+    CHECK_EQ(B->get_height(), 0);
+    CHECK_UNARY(B->get_board() == Board{"102345678"});
+    CHECK_EQ(B->get_move_dir(), MoveDir::RIGHT);
+    CHECK_EQ(B->get_move_cost(), 1);
+    CHECK_EQ(B->get_path_cost(), 1);
+    CHECK_UNARY_FALSE(B->is_root());
+    CHECK_UNARY_FALSE(B->has_children());
+    CHECK_UNARY(B->is_leaf());
+  }
+
+  SUBCASE("C-node fields valid") {
+    CHECK_EQ(C->get_parent(), A);
+    CHECK_EQ(C->get_depth(), 1);
+    CHECK_EQ(C->get_height(), 2);
+    CHECK_UNARY(C->get_board() == Board{"312045678"});
+    CHECK_EQ(C->get_move_dir(), MoveDir::DOWN);
+    CHECK_EQ(C->get_move_cost(), 3);
+    CHECK_EQ(C->get_path_cost(), 3);
+    CHECK_UNARY_FALSE(C->is_root());
+    CHECK_UNARY(C->has_children());
+    CHECK_UNARY_FALSE(C->is_leaf());
+  }
+
+  SUBCASE("D-node fields valid") {
+    CHECK_EQ(D->get_parent(), C);
+    CHECK_EQ(D->get_depth(), 2);
+    CHECK_EQ(D->get_height(), 1);
+    CHECK_UNARY(D->get_board() == Board{"312405678"});
+    CHECK_EQ(D->get_move_dir(), MoveDir::RIGHT);
+    CHECK_EQ(D->get_move_cost(), 4);
+    CHECK_EQ(D->get_path_cost(), 7);
+    CHECK_UNARY_FALSE(D->is_root());
+    CHECK_UNARY(D->has_children());
+    CHECK_UNARY_FALSE(D->is_leaf());
+  }
+
+  SUBCASE("E-node fields valid") {
+    CHECK_EQ(E->get_parent(), C);
+    CHECK_EQ(E->get_depth(), 2);
+    CHECK_EQ(E->get_height(), 0);
+    CHECK_UNARY(E->get_board() == Board{"312645078"});
+    CHECK_EQ(E->get_move_dir(), MoveDir::DOWN);
+    CHECK_EQ(E->get_move_cost(), 6);
+    CHECK_EQ(E->get_path_cost(), 9);
+    CHECK_UNARY_FALSE(E->is_root());
+    CHECK_UNARY_FALSE(E->has_children());
+    CHECK_UNARY(E->is_leaf());
+  }
+
+  SUBCASE("F-node fields valid") {
+    CHECK_EQ(F->get_parent(), D);
+    CHECK_EQ(F->get_depth(), 3);
+    CHECK_EQ(F->get_height(), 0);
+    CHECK_UNARY(F->get_board() == Board{"302415678"});
+    CHECK_EQ(F->get_move_dir(), MoveDir::UP);
+    CHECK_EQ(F->get_move_cost(), 1);
+    CHECK_EQ(F->get_path_cost(), 8);
+    CHECK_UNARY_FALSE(F->is_root());
+    CHECK_UNARY_FALSE(F->has_children());
+    CHECK_UNARY(F->is_leaf());
+  }
+
+}
+
