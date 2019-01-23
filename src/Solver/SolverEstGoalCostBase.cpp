28e081c6dde822f3773f82e6a947387e5f662d89
digimokan <digimokan@users.noreply.github.com>
2019-01-23 00:10:12 -0600

    Add base EstCost solver for uniform/f(n) solvers

---
+++ src/Solver/SolverEstGoalCostBase.cpp
@@ -0,0 +1,84 @@
+/*******************************************************************************
+module:   SolverEstGoalCostBase
+author:   digimokan
+date:     20 JAN 2019
+purpose:  base Solver impl class for BFS/DFS Solvers
+*******************************************************************************/
+
+/*******************************************************************************
+* SYSTEM INCLUDES
+*******************************************************************************/
+
+#include <memory>
+#include <optional>
+
+/*******************************************************************************
+* USER INCLUDES
+*******************************************************************************/
+
+#include "Board.hpp"
+#include "EstGoalCostIface.hpp"
+#include "FrontierQueueIface.hpp"
+#include "MoveCostIface.hpp"
+#include "SearchNode.hpp"
+#include "Solution.hpp"
+#include "SolverEstGoalCostBase.hpp"
+
+/*******************************************************************************
+* CONSTRUCTORS
+*******************************************************************************/
+
+SolverEstGoalCostBase::SolverEstGoalCostBase (Board start_board, Board goal_board,
+      const std::shared_ptr<MoveCostIface>& move_cost,
+      std::shared_ptr<EstGoalCostIface> est_cost,
+      std::shared_ptr<FrontierQueueIface> frontier_queue,
+      std::optional<size_t> max_search_depth)
+  : SolverBase{start_board, goal_board, move_cost, std::move(frontier_queue)},
+    max_search_depth{max_search_depth},
+    est_cost{std::move(est_cost)}
+{ }
+
+/*******************************************************************************
+* BASE / DERIVED METHODS
+*******************************************************************************/
+
+Solution SolverEstGoalCostBase::solve () {
+  while (true) {
+    if (this->fq_is_empty())
+      return this->make_solution_from_goal_node();
+    auto node{ this->fq_pop() };
+    if (this->is_goal(node)) {
+      this->set_goal_node(node);
+      return this->make_solution_from_goal_node();
+    }
+    this->add_to_history(node);
+    this->expand_frontier(node);
+  }
+}
+
+void SolverEstGoalCostBase::act_on_expanded_node (const std::shared_ptr<SearchNode>& enode) {
+  if (this->fq_contains(enode->get_board()))
+    this->keep_lowest_cost_fq_node(enode, this->fq_get_node(enode->get_board()).value());
+  else if (this->not_in_history(enode->get_board()))
+    this->push_to_fq_on_depth_limit(enode);
+}
+
+/*******************************************************************************
+* HELPER METHODS
+*******************************************************************************/
+
+void SolverEstGoalCostBase::keep_lowest_cost_fq_node (const std::shared_ptr<SearchNode>& exp_node,
+    const std::shared_ptr<SearchNode>& fq_node) {
+  if (exp_node->get_path_cost() < fq_node->get_path_cost()) {
+    this->fq_remove_node(fq_node);
+    this->fq_push(exp_node);
+  }
+}
+
+void SolverEstGoalCostBase::push_to_fq_on_depth_limit (const std::shared_ptr<SearchNode>& exp_node) {
+  if (! this->max_search_depth.has_value())
+    this->fq_push(exp_node);
+  else if (exp_node->get_depth() < this->max_search_depth.value())
+    this->fq_push(exp_node);
+}
+
---
+++ src/Solver/SolverEstGoalCostBase.hpp
@@ -0,0 +1,84 @@
+/*******************************************************************************
+module:   SolverEstGoalCostBase
+author:   digimokan
+date:     22 JAN 2019
+purpose:  base Solver impl class for uniform-cost or f(n) Solvers
+*******************************************************************************/
+
+#ifndef SOLVER_EST_GOAL_COST_BASE_HPP
+#define SOLVER_EST_GOAL_COST_BASE_HPP 1
+
+/*******************************************************************************
+* SYSTEM INCLUDES
+*******************************************************************************/
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+
+/*******************************************************************************
+* USER INCLUDES
+*******************************************************************************/
+
+#include "Solution.hpp"
+#include "SolverBase.hpp"
+
+/*******************************************************************************
+* FORWARD DECLARES
+*******************************************************************************/
+
+class Board;
+class EstGoalCostIface;
+class FrontierQueueIface;
+class MoveCostIface;
+class SearchNode;
+
+/*******************************************************************************
+* INTERFACE
+*******************************************************************************/
+
+class SolverEstGoalCostBase : public SolverBase  {
+
+public:
+
+  // constructors
+  SolverEstGoalCostBase () = delete;
+
+  // destructor
+  ~SolverEstGoalCostBase () override = default;
+
+  // operators
+  SolverEstGoalCostBase (const SolverEstGoalCostBase& in) = delete;
+  SolverEstGoalCostBase& operator= (const SolverEstGoalCostBase& rh) = delete;
+  SolverEstGoalCostBase (SolverEstGoalCostBase&& in) = delete;
+  SolverEstGoalCostBase& operator= (SolverEstGoalCostBase&& rh) = delete;
+
+  // base / derived methods
+  Solution solve () final;
+
+protected:
+
+  // constructors
+  explicit SolverEstGoalCostBase (Board start_board, Board goal_board, const std::shared_ptr<MoveCostIface>& move_cost, std::shared_ptr<EstGoalCostIface> est_cost, std::shared_ptr<FrontierQueueIface> frontier_queue, std::optional<size_t> max_search_depth = std::nullopt);
+
+private:
+
+  // fields
+  const std::optional<size_t> max_search_depth;
+  const std::shared_ptr<EstGoalCostIface> est_cost;
+
+  // base / derived methods
+  void act_on_expanded_node (const std::shared_ptr<SearchNode>& enode) final;
+
+  // helper methods
+  void keep_lowest_cost_fq_node (const std::shared_ptr<SearchNode>& exp_node, const std::shared_ptr<SearchNode>& fq_node);
+  void push_to_fq_on_depth_limit (const std::shared_ptr<SearchNode>& exp_node);
+
+};
+
+/*******************************************************************************
+* END
+*******************************************************************************/
+
+#endif // SOLVER_EST_GOAL_COST_BASE_HPP
+
